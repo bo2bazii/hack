c9597bfc27907788760354178f118105189e5fc8
bo2bazii <55028263+bo2bazii@users.noreply.github.com>
2019-09-07 20:10:39 +0300

    Update hack.py

--- hack.py
+++ hack.py
@@ -24,7 +24,7 @@ def Input(text):
 
 #The main class
 class Instabrute():
-	def __init__(self, username, passwordsFile='pass.txt'):
+	def __init__(self, username, passwordsFile='new.txt'):
 		self.username = username
 		self.CurrentProxy = ''
 		self.UsedProxys = []
